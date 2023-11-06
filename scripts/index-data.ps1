d4c3aa308a92ed068f5651b2663ff1427a3aaed5
sinedied <noda@free.fr>
2023-11-06 15:35:36 +0100

    feat: add script to index files

---
+++ scripts/index-data.ps1
@@ -0,0 +1,22 @@
+$scriptPath = $MyInvocation.MyCommand.Path
+cd $scriptPath/..
+
+Write-Host "Loading azd .env file from current environment"
+$output = azd env get-values
+
+foreach ($line in $output) {
+  if (!$line.Contains('=')) {
+    continue
+  }
+
+  $name, $value = $line.Split("=")
+  $value = $value -replace '^\"|\"$'
+  [Environment]::SetEnvironmentVariable($name, $value)
+}
+
+Write-Host 'Installing dependencies and building CLI'
+npm ci
+npm run build --workspace=indexer
+
+Write-Host 'Running "index-files" CLI tool'
+npx index-files --wait --indexer-url "$env:INDEXER_API_URI" --index-name "$env:AZURE_SEARCH_INDEX" ./data/*.md
---
+++ scripts/index-data.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
+
+echo "Loading azd .env file from current environment"
+export $(azd env get-values | xargs)
+
+echo 'Installing dependencies and building CLI'
+npm ci
+npm run build --workspace=indexer
+
+echo 'Running "index-files" CLI tool'
+npx index-files \
+  --wait \
+  --indexer-url "${INDEXER_API_URI}" \
+  --index-name "${AZURE_SEARCH_INDEX}" \
+  ./data/*.md
