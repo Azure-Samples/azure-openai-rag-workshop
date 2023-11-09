308ba315455cb211dd68bf567a1bae246b2de9bc
sinedied <noda@free.fr>
2023-11-09 16:45:16 +0100

    fix: frontend template

--- scripts/repo/create-github-template.sh
+++ scripts/repo/create-github-template.sh
@@ -114,7 +114,7 @@ declare module 'fastify' {
 
 echo -e "import { type ChatResponse, type ChatRequestOptions, type ChatResponseChunk } from './models.js';
 
-export const apiBaseUrl = import.meta.env.VITE_CHAT_API_URI || '';
+export const apiBaseUrl = import.meta.env.VITE_BACKEND_API_URI || '';
 
 export async function getCompletion(options: ChatRequestOptions, oneShot = false) {
   const apiUrl = options.apiUrl || apiBaseUrl;
