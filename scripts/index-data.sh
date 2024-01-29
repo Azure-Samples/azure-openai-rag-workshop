6a381cd2e6d3523e2102be12699fb6940ab57b56
sinedied <noda@free.fr>
2024-01-29 15:47:59 +0100

    fix: index script

--- scripts/index-data.sh
+++ scripts/index-data.sh
@@ -3,7 +3,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 if azd_env=$(azd env get-values); then
   echo "Loading azd .env file from current environment"
-  export "$(echo "$azd_env" | xargs)"
+  export $(echo "$azd_env" | xargs)
 fi
 
 echo 'Installing dependencies and building CLI'
