04e39d5590ac2d91bc89f5dffe07f4fb089cf4d2
Antonio Goncalves <antonio.goncalves@gmail.com>
2024-02-14 11:36:46 +0100

    Initialize Quarkus app

---
+++ src/backend-java-quarkus/src/test/java/ai/azure/openai/rag/workshop/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package ai.azure.openai.rag.workshop;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
