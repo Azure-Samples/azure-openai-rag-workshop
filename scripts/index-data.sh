cc0a7ec233819123bc2ad8d8e2361f4f579dd9a7
Yohan Lasorsa <noda@free.fr>
2024-04-02 14:39:16 +0000

    chore: ingestion script wip

--- scripts/index-data.sh
+++ scripts/index-data.sh
@@ -6,6 +6,8 @@ if azd_env=$(azd env get-values); then
   export $(echo "$azd_env" | xargs)
 fi
 
+api_mode=false
+
 echo 'Installing dependencies and building CLI'
 npm install
 npm run build --workspace=ingestion
@@ -16,3 +18,8 @@ npx index-files \
   --ingestion-url "${INGESTION_API_URI:-http://localhost:3001}" \
   --index-name "${INDEX_NAME:-kbindex}" \
   ./data/*.pdf
+
+curl -F "file=@./data/privacy-policy.pdf" \
+  -F "file=@./data/support.pdf" \
+  -F "file=@./data/terms-of-service.pdf" \
+  "${INGESTION_API_URI:-http://localhost:3001}/ingest"
