976f478e39e23c721f388aa19e73ef724bcfb22e
sinedied <noda@free.fr>
2024-04-02 16:44:45 +0200

    ci: add package for java

--- scripts/repo/create-packages.sh
+++ scripts/repo/create-packages.sh
@@ -54,12 +54,14 @@ rm -rf "$target_folder/solution/trainer"
 rm -rf "$target_folder/solution/scripts/repo"
 rm -rf "$target_folder/solution/.github"
 rm -rf "$target_folder/solution/src/backend-"*
+rm -rf "$target_folder/solution/src/ingestion-"*
 rm -rf "$target_folder/solution/TODO"
 rm -rf "$target_folder/solution/SUPPORT.md"
 rm -rf "$target_folder/solution/CODE_OF_CONDUCT.md"
 rm -rf "$target_folder/solution/SECURITY.md"
 rm -rf "$target_folder/solution/docker-compose.yml"
 rm -rf "$target_folder/solution/scripts/setup-template.sh"
+rm -rf "$target_folder/solution/pom.xml"
 perl -pi -e 's/stream: false/stream: true/g' "$target_folder/solution/src/frontend/src/components/chat.ts"
 makeArchive . solution solution
 
@@ -75,13 +77,38 @@ rm -rf "$target_folder/solution-qdrant/trainer"
 rm -rf "$target_folder/solution-qdrant/scripts/repo"
 rm -rf "$target_folder/solution-qdrant/.github"
 rm -rf "$target_folder/solution-qdrant/src/backend-"*
+rm -rf "$target_folder/solution-qdrant/src/ingestion-"*
 rm -rf "$target_folder/solution-qdrant/TODO"
 rm -rf "$target_folder/solution-qdrant/SUPPORT.md"
 rm -rf "$target_folder/solution-qdrant/CODE_OF_CONDUCT.md"
 rm -rf "$target_folder/solution-qdrant/SECURITY.md"
 rm -rf "$target_folder/solution-qdrant/scripts/setup-template.sh"
+rm -rf "$target_folder/solution/pom.xml"
 perl -pi -e 's/stream: false/stream: true/g' "$target_folder/solution-qdrant/src/frontend/src/components/chat.ts"
 makeArchive . solution-qdrant solution-qdrant
+
+echo "Creating solution package (for Java + Quarkus)..."
+copyFolder . solution-java-quarkus
+mv "$target_folder/solution-java-quarkus/src/backend-node-qdrant" "$target_folder/solution-java-quarkus/src/backend"
+rm -rf "$target_folder/solution-java-quarkus/src/ingestion"
+mv "$target_folder/solution-java-quarkus/src/ingestion-java" "$target_folder/solution-java-quarkus/src/ingestion"
+rm -rf "$target_folder/solution-java-quarkus/.azure"
+rm -rf "$target_folder/solution-java-quarkus/.qdrant"
+rm -rf "$target_folder/solution-java-quarkus/.env"
+rm -rf "$target_folder/solution-java-quarkus/.env*"
+rm -rf "$target_folder/solution-java-quarkus/docs"
+rm -rf "$target_folder/solution-java-quarkus/trainer"
+rm -rf "$target_folder/solution-java-quarkus/scripts/repo"
+rm -rf "$target_folder/solution-java-quarkus/.github"
+rm -rf "$target_folder/solution-java-quarkus/src/backend-"*
+rm -rf "$target_folder/solution-java-quarkus/src/ingestion-"*
+rm -rf "$target_folder/solution-java-quarkus/TODO"
+rm -rf "$target_folder/solution-java-quarkus/SUPPORT.md"
+rm -rf "$target_folder/solution-java-quarkus/CODE_OF_CONDUCT.md"
+rm -rf "$target_folder/solution-java-quarkus/SECURITY.md"
+rm -rf "$target_folder/solution-java-quarkus/scripts/setup-template.sh"
+perl -pi -e 's/stream: false/stream: true/g' "$target_folder/solution-java-quarkus/src/frontend/src/components/chat.ts"
+makeArchive . solution-java-quarkus solution-java-quarkus
 
 ##############################################################################
 # Backend Dockerfile
