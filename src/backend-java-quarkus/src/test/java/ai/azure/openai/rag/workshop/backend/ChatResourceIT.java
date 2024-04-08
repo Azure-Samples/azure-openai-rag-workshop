80593a4f8ef441c081b151bad845a0c5e4e535ee
Yohan Lasorsa <noda@free.fr>
2024-04-08 08:40:24 +0000

    chore: remove tests

--- src/backend-java-quarkus/src/test/java/ai/azure/openai/rag/workshop/backend/ChatResourceIT.java
+++
@@ -1,8 +0,0 @@
-package ai.azure.openai.rag.workshop.backend;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class ChatResourceIT extends ChatResourceTest {
-    // Execute the same tests but in packaged mode.
-}
