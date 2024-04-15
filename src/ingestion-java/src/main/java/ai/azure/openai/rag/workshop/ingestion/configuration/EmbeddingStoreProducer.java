e24f5fa2c2822386d64dc78657a9fb4947e3dd5f
Julien Dubois <julien.dubois@gmail.com>
2024-04-15 16:29:32 +0200

    Create the QDrant collection in Java

--- src/ingestion-java/src/main/java/ai/azure/openai/rag/workshop/ingestion/configuration/EmbeddingStoreProducer.java
+++ src/ingestion-java/src/main/java/ai/azure/openai/rag/workshop/ingestion/configuration/EmbeddingStoreProducer.java
@@ -3,13 +3,24 @@ package ai.azure.openai.rag.workshop.ingestion.configuration;
 import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.store.embedding.EmbeddingStore;
 import dev.langchain4j.store.embedding.qdrant.QdrantEmbeddingStore;
+import io.qdrant.client.QdrantClient;
+import io.qdrant.client.QdrantGrpcClient;
+import io.qdrant.client.grpc.Collections.CollectionOperationResponse;
+import io.qdrant.client.grpc.Collections.Distance;
+import io.qdrant.client.grpc.Collections.VectorParams;
 import jakarta.enterprise.inject.Produces;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.util.concurrent.ListenableFuture;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 
 public class EmbeddingStoreProducer {
+
+  private static final Logger log = LoggerFactory.getLogger(EmbeddingStoreProducer.class);
 
   @ConfigProperty(name = "AZURE_SEARCH_INDEX", defaultValue = "kbindex")
   String azureSearchIndexName;
@@ -21,10 +32,24 @@ public class EmbeddingStoreProducer {
   public EmbeddingStore<TextSegment> embeddingStore() throws URISyntaxException {
     String qdrantHostname = new URI(qdrantUrl).getHost();
     int qdrantPort = new URI(qdrantUrl).getPort();
+
+    QdrantGrpcClient.Builder grpcClientBuilder = QdrantGrpcClient.newBuilder(qdrantHostname, qdrantPort, false);
+    QdrantClient qdrantClient = new QdrantClient(grpcClientBuilder.build());
+
+    ListenableFuture<CollectionOperationResponse> futureCollection = qdrantClient.createCollectionAsync(azureSearchIndexName, VectorParams.newBuilder().setSize(384).setDistance(Distance.Cosine).build());
+
+    while (!futureCollection.isDone()) {
+      try {
+        log.info("Creating collection {}", azureSearchIndexName);
+        Thread.sleep(1000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+
     return QdrantEmbeddingStore.builder()
+      .client(qdrantClient)
       .collectionName(azureSearchIndexName)
-      .host(qdrantHostname)
-      .port(qdrantPort)
       .build();
   }
 }
