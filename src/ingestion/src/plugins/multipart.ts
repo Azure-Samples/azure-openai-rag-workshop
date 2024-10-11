19efc8f76a40a1d952da25d03ae5c2507287f374
sinedied <noda@free.fr>
2024-10-11 15:27:40 +0200

    feat: update ingestion server

--- src/ingestion/src/plugins/multipart.ts
+++ src/ingestion/src/plugins/multipart.ts
@@ -1,9 +1,9 @@
 import fp from 'fastify-plugin';
-import multipart from '@fastify/multipart';
+import multipart, { FastifyMultipartOptions } from '@fastify/multipart';
 
 const FILE_UPLOAD_LIMIT = 20 * 1024 * 1024; // 20 MB
 
-export default fp(async (fastify) => {
+export default fp<FastifyMultipartOptions>(async (fastify) => {
   fastify.register(multipart, {
     attachFieldsToBody: true,
     sharedSchemaId: 'multipartField',
